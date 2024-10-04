702ef7c234fddb2f85b12195ac789028f1dd2f98
pugeaale <pugeaale@gmail.com>
2024-10-04 14:08:57 +0200

    :sparkles: implementing exposition service

---
+++ task2/src/main/java/com/ironhack/task2/service/ExpositionService.java
@@ -0,0 +1,18 @@
+package com.ironhack.task2.service;
+
+import com.ironhack.task2.model.Exposition;
+import com.ironhack.task2.repository.ExpositionRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpositionService {
+    private final ExpositionRepository expositionRepository;
+
+    @Transactional
+    public Exposition save(Exposition exposition) {
+        return expositionRepository.save(exposition);
+    }
+}
