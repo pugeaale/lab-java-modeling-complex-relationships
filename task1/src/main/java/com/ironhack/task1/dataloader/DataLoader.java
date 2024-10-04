066a8af4777a4d405addaa697dc70343d6263ffd
pugeaale <pugeaale@gmail.com>
2024-10-04 10:28:42 +0200

    :sparkles: implementing create chapter and president with datas

--- task1/src/main/java/com/ironhack/task1/dataloader/DataLoader.java
+++ task1/src/main/java/com/ironhack/task1/dataloader/DataLoader.java
@@ -22,24 +22,30 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Faker faker = new Faker();
-
-        Chapter chapter = new Chapter(
-                faker.address().cityName(),
-                faker.funnyName().name());
-
-        Member president = new Member(
-                faker.name().fullName(),
-                ZonedDateTime.now(),
-                MemberStatus.ACTIVE);
-
-        chapter.setPresident(president);
-        chapterService.save(chapter);
-
+        create7chaptersWithPresidents();
         Member member = new Member(
                 faker.name().femaleFirstName(),
                 ZonedDateTime.now(),
                 MemberStatus.ACTIVE);
         memberService.save(member);
+
+    }
+
+    public void create7chaptersWithPresidents() {
+        Faker faker = new Faker();
+        for(int i = 0; i < 7; i++) {
+            Chapter chapter = new Chapter(
+                    faker.address().cityName(),
+                    faker.funnyName().name());
+
+            Member president = new Member(
+                    faker.name().fullName(),
+                    ZonedDateTime.now(),
+                    MemberStatus.ACTIVE);
+
+            chapter.setPresident(president);
+            chapterService.save(chapter);
+        }
 
     }
 }
