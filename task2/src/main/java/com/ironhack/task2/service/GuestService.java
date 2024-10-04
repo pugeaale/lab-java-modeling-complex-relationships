c736e2daa8eefa707c594aa4060e97edc3d9d8e3
pugeaale <pugeaale@gmail.com>
2024-10-04 18:39:42 +0200

    :sparkles: implementing findAll and deleteById for guest model

--- task2/src/main/java/com/ironhack/task2/service/GuestService.java
+++ task2/src/main/java/com/ironhack/task2/service/GuestService.java
@@ -2,15 +2,28 @@ package com.ironhack.task2.service;
 
 import com.ironhack.task2.model.Guest;
 import com.ironhack.task2.repository.GuestRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GuestService {
     private final GuestRepository guestRepository;
 
+    @Transactional
     public Guest save(Guest guest) {
         return guestRepository.save(guest);
+    }
+
+    public List<Guest> findAll() {
+        return guestRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        guestRepository.deleteById(id);
     }
 }
