b9ad806f5d0ae358b5882ff68142391548efa488
pugeaale <pugeaale@gmail.com>
2024-10-04 11:46:15 +0200

    :sparkles: implementing guest repository

---
+++ task2/src/main/java/com/ironhack/task2/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.ironhack.task2.repository;
+
+import com.ironhack.task2.model.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
