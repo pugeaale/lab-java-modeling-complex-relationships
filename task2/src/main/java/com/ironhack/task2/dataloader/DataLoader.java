0fb2341f48e8e4914543c2b63736822cdda695b0
pugeaale <pugeaale@gmail.com>
2024-10-04 11:51:00 +0200

    :sparkles: loading new data with creating new guest

--- task2/src/main/java/com/ironhack/task2/dataloader/DataLoader.java
+++ task2/src/main/java/com/ironhack/task2/dataloader/DataLoader.java
@@ -1,6 +1,9 @@
 package com.ironhack.task2.dataloader;
 
+import com.ironhack.task2.model.Guest;
+import com.ironhack.task2.model.GuestStatus;
 import com.ironhack.task2.model.Speaker;
+import com.ironhack.task2.service.GuestService;
 import com.ironhack.task2.service.SpeakerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -10,12 +13,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
     private final SpeakerService speakerService;
+    private final GuestService guestService;
 
     @Override
     public void run(String... args) throws Exception {
         Speaker speaker = new Speaker();
-        speaker.setName("ddd");
+        speaker.setName("warren");
         speaker.setPresentationDuration(33);
         speakerService.save(speaker);
+
+        Guest guest = new Guest();
+        guest.setName("azer");
+        guest.setStatus(GuestStatus.ATTENDING);
+        guestService.save(guest);
     }
 }
