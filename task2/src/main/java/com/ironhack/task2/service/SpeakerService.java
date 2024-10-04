f03e92b784ffd9476b4a9fc24f099ca97f67b36b
pugeaale <pugeaale@gmail.com>
2024-10-04 18:40:02 +0200

    :sparkles: implementing findAll and deleteById for speaker model

--- task2/src/main/java/com/ironhack/task2/service/SpeakerService.java
+++ task2/src/main/java/com/ironhack/task2/service/SpeakerService.java
@@ -2,15 +2,28 @@ package com.ironhack.task2.service;
 
 import com.ironhack.task2.model.Speaker;
 import com.ironhack.task2.repository.SpeakerRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SpeakerService {
     private final SpeakerRepository speakerRepository;
 
+    @Transactional
     public Speaker save(Speaker speaker) {
         return speakerRepository.save(speaker);
+    }
+
+    public List<Speaker> findAll() {
+        return speakerRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        speakerRepository.deleteById(id);
     }
 }
