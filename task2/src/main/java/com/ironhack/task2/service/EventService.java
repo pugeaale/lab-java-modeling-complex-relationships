dd8c1812390ee9a56b54e4783666f4647ac3587d
pugeaale <pugeaale@gmail.com>
2024-10-04 13:34:56 +0200

    :sparkles: implementing service for event with save implementation

---
+++ task2/src/main/java/com/ironhack/task2/service/EventService.java
@@ -0,0 +1,18 @@
+package com.ironhack.task2.service;
+
+import com.ironhack.task2.model.Event;
+import com.ironhack.task2.repository.EventRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    @Transactional
+    public Event save(Event event) {
+        return eventRepository.save(event);
+    }
+}
