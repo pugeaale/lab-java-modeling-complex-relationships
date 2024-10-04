dc371423816b7a2c32d0d1369d3e46b553c8c1f9
pugeaale <pugeaale@gmail.com>
2024-10-04 14:17:37 +0200

    :sparkles: implementing conference service

---
+++ task2/src/main/java/com/ironhack/task2/service/ConferenceService.java
@@ -0,0 +1,18 @@
+package com.ironhack.task2.service;
+
+import com.ironhack.task2.model.Conference;
+import com.ironhack.task2.repository.ConferenceRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConferenceService {
+    private final ConferenceRepository conferenceRepository;
+
+    @Transactional
+    public Conference save(Conference conference) {
+        return conferenceRepository.save(conference);
+    }
+}
