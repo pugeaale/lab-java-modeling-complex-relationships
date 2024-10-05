5be613d242abff599d0f3b8c433ba7d6514fbcf2
pugeaale <pugeaale@gmail.com>
2024-10-05 09:50:14 +0200

    :sparkles: implementing find by id for conference

--- task2/src/main/java/com/ironhack/task2/service/ConferenceService.java
+++ task2/src/main/java/com/ironhack/task2/service/ConferenceService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ConferenceService {
@@ -18,5 +20,9 @@ public class ConferenceService {
 
     public long count() {
         return conferenceRepository.count();
+    }
+
+    public Optional<Conference> findById(long id) {
+        return conferenceRepository.findById(id);
     }
 }
