63054acf1cafa0aec7f18d7f3daa8bb705d9ade9
pugeaale <pugeaale@gmail.com>
2024-10-05 14:56:11 +0200

    :art: implementing test for crud operations

--- task1/src/main/java/com/ironhack/task1/dataloader/DataLoader.java
+++ task1/src/main/java/com/ironhack/task1/dataloader/DataLoader.java
@@ -10,6 +10,7 @@ import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -19,21 +20,97 @@ public class DataLoader implements CommandLineRunner {
 
     private final ChapterService chapterService;
 
+    private Faker faker = new Faker();
+
     @Override
     public void run(String... args) throws Exception {
-        //create7chaptersWithPresidents();
+        testCreateChapterWithoutPresident();
+        testFindChapterById();
+        testDeleteChapterById();
+
+        testCreateMemberWithoutChapter();
+        testFindMemberById();
+        testDeleteMemberById();
+
+        testCreateMemberWithChapter();
+        testCreateChapterWithPresident();
+    }
+
+    private void testDeleteMemberById() {
+        memberService.deleteById(1L);
+        Optional<Member> opt = memberService.findById(1L);
+        System.out.println(opt.isEmpty());
+    }
+
+    private void testFindMemberById() {
+        Optional<Member> opt = memberService.findById(1L);
+        System.out.println(opt.isPresent());
+    }
+
+    private void testDeleteChapterById() {
+        chapterService.deleteById(1L);
+        Optional<Chapter> opt = chapterService.findById(1L);
+        System.out.println(opt.isEmpty());
+    }
+
+    private void testFindChapterById() {
+        Optional<Chapter> opt = chapterService.findById(1L);
+        System.out.println(opt.isPresent());
+    }
+
+    public void testCreateChapterWithoutPresident() {
+        Chapter chapter = new Chapter(
+                faker.address().city(),
+                faker.name().fullName());
+        Chapter chapterSaved = chapterService.save(chapter);
+        System.out.println(chapterSaved.getName().equals(chapter.getName()));
+    }
+
+    public void testCreateMemberWithoutChapter() {
         Member member = new Member(
-            "dd",
+                faker.name().fullName(),
                 ZonedDateTime.now(),
-                MemberStatus.ACTIVE
-        );
-        Chapter chapter = new Chapter("d","dd");
+                MemberStatus.ACTIVE);
+        Member memberSaved = memberService.save(member);
+        System.out.println(memberSaved.getName().equals(member.getName()));
+
+    }
+
+    public void testCreateMemberWithChapter() {
+        Member member = new Member(
+                faker.name().fullName(),
+                ZonedDateTime.now(),
+                MemberStatus.ACTIVE);
+        Chapter chapter = new Chapter(
+                faker.address().city(),
+                faker.name().fullName());
         member.setChapter(chapter);
-        memberService.save(member);
+        Member memberSaved = memberService.save(member);
+        System.out.println("testCreateMemberWithChapter : " +
+                memberSaved.getName().equals(member.getName()) + " - " +
+                memberSaved.getChapter().getName().equals(chapter.getName())
+        );
+
+    }
+
+    public void testCreateChapterWithPresident() {
+        Chapter chapter = new Chapter(
+                faker.address().city(),
+                faker.name().fullName());
+        Member president = new Member(
+                faker.name().fullName(),
+                ZonedDateTime.now(),
+                MemberStatus.ACTIVE);
+        chapter.setPresident(president);
+        Chapter chapterSaved = chapterService.save(chapter);
+        System.out.println("testCreateChapterWithPresident : " +
+                chapterSaved.getName().equals(chapter.getName()) + " - " +
+                chapterSaved.getPresident().getName().equals(president.getName())
+        );
     }
 
     public void create7chaptersWithPresidents() {
-        Faker faker = new Faker();
+
         for(int i = 0; i < 1; i++) {
             Chapter chapter = new Chapter(
                     faker.address().cityName(),
@@ -50,7 +127,7 @@ public class DataLoader implements CommandLineRunner {
                     faker.name().femaleFirstName(),
                     ZonedDateTime.now(),
                     MemberStatus.ACTIVE);
-            chapter.getMembers().add(member);
+            chapter.addMember(member);
 
             chapterService.save(chapter);
         }
