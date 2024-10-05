665901c252773d21a881c4601d18149d8a408ba3
pugeaale <pugeaale@gmail.com>
2024-10-05 14:41:00 +0200

    :sparkles: implementing find by id and delete by id for chapter

--- task1/src/main/java/com/ironhack/task1/service/ChapterService.java
+++ task1/src/main/java/com/ironhack/task1/service/ChapterService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -18,5 +20,16 @@ public class ChapterService {
     public Chapter save(Chapter chapter) {
         log.info("Saving chapter: {}", chapter);
         return chapterRepository.save(chapter);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        log.info("Deleting chapter: {}", id);
+        chapterRepository.deleteById(id);
+    }
+
+    public Optional<Chapter> findById(Long id) {
+        log.info("Finding chapter: {}", id);
+        return chapterRepository.findById(id);
     }
 }
