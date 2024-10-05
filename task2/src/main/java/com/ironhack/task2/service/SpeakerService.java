ee2fdd79889b3c23875618a77bc8736bfc7219c4
pugeaale <pugeaale@gmail.com>
2024-10-05 09:52:17 +0200

    :sparkles: implementing find by id for speaker

--- task2/src/main/java/com/ironhack/task2/service/SpeakerService.java
+++ task2/src/main/java/com/ironhack/task2/service/SpeakerService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,9 @@ public class SpeakerService {
     @Transactional
     public void deleteById(Long id) {
         speakerRepository.deleteById(id);
+    }
+
+    public Optional<Speaker> findById(Long id) {
+        return speakerRepository.findById(id);
     }
 }
