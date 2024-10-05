628f9c5ab3203f2a1b89a05783be986b45507899
pugeaale <pugeaale@gmail.com>
2024-10-05 14:43:21 +0200

    :sparkles: implementing find by id and delete by id for member

--- task1/src/main/java/com/ironhack/task1/service/MemberService.java
+++ task1/src/main/java/com/ironhack/task1/service/MemberService.java
@@ -1,11 +1,14 @@
 package com.ironhack.task1.service;
 
+import com.ironhack.task1.model.Chapter;
 import com.ironhack.task1.model.Member;
 import com.ironhack.task1.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,16 @@ public class MemberService {
     public Member save(Member member) {
         log.info("Saving member {}", member);
         return memberRepository.save(member);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        log.info("Deleting member: {}", id);
+        memberRepository.deleteById(id);
+    }
+
+    public Optional<Member> findById(Long id) {
+        log.info("Finding member: {}", id);
+        return memberRepository.findById(id);
     }
 }
